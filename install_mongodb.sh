a251f53ced6a0ad0385c02411668166a8a497f4a
vsit <xwwww7@gmail.com>
2023-04-30 02:15:10 +0500

    manual start

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -6,4 +6,4 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-mongod --fork --syslog
+sudo start mongod
