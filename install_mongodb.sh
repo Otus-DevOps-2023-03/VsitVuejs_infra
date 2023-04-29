7f4b96421d2373a4641ec6a3a2d63e76d9edc81d
vsit <xwwww7@gmail.com>
2023-04-30 00:30:51 +0500

    add install pkg

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo apt-get install -y apt-transport-https
-sudo apt-get clean
-sudo pkexec synaptic
-sudo apt-get install -f
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
+sudo apt-get install -y mongodb-org-shell
+sudo apt-get install -y mongodb-org-server
+sudo apt-get install -y mongodb-org-mongos
 sudo apt-get install -y mongodb-org
 sudo apt-get install libc6
 sudo service mongod start
