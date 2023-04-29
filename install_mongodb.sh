ef5253fe9e2cbb364c5bb0fb038441a5213aec1b
vsit <xwwww7@gmail.com>
2023-04-30 00:26:10 +0500

    list broken pkg

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt-get install -y apt-transport-https
-dpkg -l | grep mongo
-sudo apt-get purge mongodb-org*
-sudo dpkg -r mongodb-org
+sudo apt-get clean
+sudo pkexec synaptic
+sudo apt-get install -f
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
