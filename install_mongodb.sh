f3d938c0e57bd4ebc93449f9feb024631e9f21e1
vsit <xwwww7@gmail.com>
2023-04-29 23:39:32 +0500

    remove and install mongodb

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-sudo apt-get install apt-transport-https ca-certificates
-curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+sudo apt-get install -y apt-transport-https
+sudo apt-get purge mongodb-org*
+sudo apt remove mongodb
+sudo apt purge mongodb
+sudo apt autoremove
+sudo rm -r /var/log/mongodb
+sudo rm -r /var/lib/mongodb
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+sudo apt-get install libc6
+sudo service mongod start
+sudo service mongod status
