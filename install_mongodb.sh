802e32cb897958f9419d29d22a22549cf886fc03
vsit <xwwww7@gmail.com>
2023-04-30 00:46:35 +0500

    install mongo ubuntu 16

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt-get install -y apt-transport-https
+sudo apt-get install -y gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc |  gpg --dearmor | sudo tee /usr/share/keyrings/mongodb.gpg > /dev/null
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo service mongod start
 sudo service mongod status
-cat /etc/os-release
