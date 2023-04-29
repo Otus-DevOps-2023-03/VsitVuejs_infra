395d61b3293897c5bc06021953efb56c7aa440ce
vsit <xwwww7@gmail.com>
2023-04-30 01:15:31 +0500

    mongodb 4.0

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install -y apt-transport-https
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
