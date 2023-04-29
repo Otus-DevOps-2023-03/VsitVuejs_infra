22db2d632f991ef6d5e09c0ca2abbd861da5894c
vsit <xwwww7@gmail.com>
2023-04-30 00:49:49 +0500

    get key for repo

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get install -y apt-transport-https
 sudo apt-get install -y gnupg
-wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc |  gpg --dearmor | sudo tee /usr/share/keyrings/mongodb.gpg > /dev/null
+curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
