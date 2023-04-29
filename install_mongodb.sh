8d191d31d5b3f0c6a95ac8a157042284de406e1a
vsit <xwwww7@gmail.com>
2023-04-30 00:38:27 +0500

    other mongo

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 sudo apt-get install -y apt-transport-https
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc |  gpg --dearmor | sudo tee /usr/share/keyrings/mongodb.gpg > /dev/null
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y mongodb-org-shell
-sudo apt-get install -y mongodb-org-server
-sudo apt-get install -y mongodb-org-mongos
 sudo apt-get install -y mongodb-org
-sudo apt-get install libc6
 sudo service mongod start
 sudo service mongod status
