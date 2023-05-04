25666e75e256eb36b256253adce2c52dbd487178
vsit <xwwww7@gmail.com>
2023-05-04 16:26:07 +0500

    create vm

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+provider "yandex" {
+  cloud_id                 = "b1gdh83qoor6p91f65je"
+  folder_id                = "b1gmldmegginvb5f7loj"
+  zone                     = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "app" {
+  name = "reddit-app"
+  platform_id = "standard-v2"
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 5
+  }
+  boot_disk {
+    initialize_params {
+      # Указать id образа созданного в предыдущем домашнем задании
+      image_id = "fd89a1legnb7jaejcfu4"
+    }
+  }
+  network_interface {
+    # Указан id подсети default-ru-central1-a
+    subnet_id = "e9bvkoffg887jttgo5ov"
+    nat       = true
+  }
+}
