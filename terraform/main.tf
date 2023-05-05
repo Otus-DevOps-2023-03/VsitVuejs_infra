5019b951d5ec1092ee84303736e816c8fdabad95
vsit <xwwww7@gmail.com>
2023-05-05 17:56:06 +0500

    add variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,26 @@
 provider "yandex" {
-  cloud_id  = "b1gdh83qoor6p91f65je"
-  folder_id = "b1gmldmegginvb5f7loj"
-  zone      = "ru-central1-a"
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
 }
 
 resource "yandex_compute_instance" "app" {
   name        = "reddit-app"
   platform_id = "standard-v2"
+  zone        = var.zone_yci
 
   metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/appuser.pub")}}"
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
 
   connection {
-    type        = "ssh"
-    host        = yandex_compute_instance.app.network_interface.0.nat_ip_address
-    user        = "ubuntu"
-    agent       = false
+    type  = "ssh"
+    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user  = "ubuntu"
+    agent = false
     # путь до приватного ключа
-    private_key = file("~/.ssh/appuser")
+    private_key = file(var.private_key_path)
   }
 
   provisioner "file" {
@@ -33,17 +35,17 @@ resource "yandex_compute_instance" "app" {
   resources {
     cores         = 2
     memory        = 2
-    core_fraction = 5
+    core_fraction = 20
   }
   boot_disk {
     initialize_params {
       # Указать id образа созданного в предыдущем домашнем задании
-      image_id = "fd8d4tnn78vqf4ungh6j"
+      image_id = var.image_id
     }
   }
   network_interface {
     # Указан id подсети default-ru-central1-a
-    subnet_id = "e9bvkoffg887jttgo5ov"
+    subnet_id = var.subnet_id
     nat       = true
   }
 }
