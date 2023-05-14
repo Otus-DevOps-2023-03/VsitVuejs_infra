ba47c8986713b12a6ac1ac51978b17fe7f97c610
vsit <xwwww7@gmail.com>
2023-05-14 21:50:12 +0500

    change var

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,8 @@
 output "external_ip_address_app" {
-  value = yandex_compute_instance.app.network_interface.0.nat_ip_address
+  value = module.app.external_ip_address_app
 }
 output "external_ip_address_db" {
-  value = yandex_compute_instance.db.network_interface.0.nat_ip_address
+  value = module.db.external_ip_address_db
 }
 #output "external_ip_address_lb" {
 #  value = tolist(tolist(yandex_lb_network_load_balancer.lb.listener)[0].external_address_spec)[0].address
