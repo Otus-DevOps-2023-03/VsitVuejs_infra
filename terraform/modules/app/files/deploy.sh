325bc356f1acf962ae0ae9d3a954b639aa52d468
vsit <xwwww7@gmail.com>
2023-05-15 02:21:03 +0500

    add environment file

--- terraform/modules/app/files/deploy.sh
+++ terraform/modules/app/files/deploy.sh
@@ -7,6 +7,5 @@ git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
 cd $APP_DIR/reddit
 bundle install
 sudo mv /tmp/puma.service /etc/systemd/system/puma.service
-sudo systemctl import-environment DATABASE_URL
 sudo systemctl start puma
 sudo systemctl enable puma
