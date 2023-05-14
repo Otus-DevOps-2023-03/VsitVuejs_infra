497ffa6bb02415671640b138242d019d42b0ef9e
vsit <xwwww7@gmail.com>
2023-05-15 02:11:51 +0500

    add environment

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -9,6 +9,8 @@ module "app" {
   public_key_path = var.public_key_path
   app_disk_image  = var.app_disk_image
   subnet_id       = var.subnet_id
+  db_ip           = module.db.external_ip_address_db
+  private_key_path = var.private_key_path
 }
 
 module "db" {
@@ -16,4 +18,5 @@ module "db" {
   public_key_path = var.public_key_path
   db_disk_image   = var.db_disk_image
   subnet_id       = var.subnet_id
+  private_key_path = var.private_key_path
 }
