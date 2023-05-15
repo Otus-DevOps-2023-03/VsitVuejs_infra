b19bde3edade298e4c6a0a4b922ca88d97f45438
vsit <xwwww7@gmail.com>
2023-05-15 12:22:44 +0500

    format file

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -1,10 +1,10 @@
 terraform {
 
   backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "my-bucket-1105"
-    region     = "ru-central1-a"
-    key        = "state.tfstate"
+    endpoint = "storage.yandexcloud.net"
+    bucket   = "my-bucket-1105"
+    region   = "ru-central1-a"
+    key      = "state.tfstate"
 
     skip_region_validation      = true
     skip_credentials_validation = true
