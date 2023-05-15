8b62b45fae46a1b802ed8656446acff1be410e72
vsit <xwwww7@gmail.com>
2023-05-15 12:20:02 +0500

    add var bucket

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable cloud_id {
+  description = "Cloud"
+}
+variable folder_id {
+  description = "Folder"
+}
+variable zone {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
